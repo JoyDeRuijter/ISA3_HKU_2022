b061729fbf38e9ed4469943fab0ee720e4fe6ac6
Joy <joyderuijter@gmail.com>
2022-06-11 22:25:52 +0200

    Create a forge cutscene

---
+++ Assets/Scripts/BucketFollow.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class BucketFollow : MonoBehaviour
+{
+    void Start()
+    {
+        transform.DOMoveZ(-380.5f, 110);
+    }
+}
